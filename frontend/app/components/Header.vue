e50ea6fd3b0b9d5b1ee36023b5dd9e2d455de4a5
Vhin <vhinlilis07@gmail.com>
2025-10-03 17:28:15 +0800

    feat: create header component

---
+++ frontend/app/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <header class="bg-white shadow p-4 flex justify-between items-center ml-64">
+    <h2 class="text-xl font-bold">Dashboard</h2>
+    <div>User Menu</div>
+  </header>
+</template>
